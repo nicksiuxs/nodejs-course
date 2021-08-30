323bb01831ee7ece4f4862a4e4a13ffd3625c043
Nicolas <nicksiuxs@gmail.com>
2021-08-29 20:54:51 -0500

    REAFACTOR: rename variables

--- tutorial_1/3-modules-app.js
+++ tutorial_1/3-modules-app.js
@@ -1,8 +1,12 @@
 // Modules
 
-const { john, peter, susan } = require("./3-1-name");
-const { sayHi } = require("./3-2-utils");
-
+const { john, peter, susan } = require("./4-name");
+const { sayHi } = require("./5-utils");
+// const { items, singlePerson } = require("./6-alternative-flavors");
+const data = require("./6-alternative-flavors");
+console.log(data);
 sayHi(susan);
 sayHi(john);
 sayHi(peter);
+// sayHi(singlePerson.name);
+// sayHi(items[0]);
